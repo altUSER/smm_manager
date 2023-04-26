878620c3cbb1bf4de48f5493aafd86686f92e319
krearden <krearden@yandex.ru>
2023-04-26 16:25:57 +0500

    backup for nickross

--- smm_manager/smm_manager/urls.py
+++ smm_manager/smm_manager/urls.py
@@ -20,5 +20,8 @@ from ui import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='home'),
+    path('delayed_posts/', views.delayed_posts, name='delayed_posts'),
+    path('published_posts/', views.published_posts, name='published_posts'),
+    path('new_publication/', views.new_publication, name='new_publication'),
+    path('settings/', views.settings, name='settings'),
 ]
--- smm_manager/ui/views.py
+++ smm_manager/ui/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'index.html')
+def delayed_posts(request):
+    return render(request, 'delayed_posts.html')
+
+def published_posts(request):
+    return render(request, 'published_posts.html')
+
+def new_publication(request):
+    return render(request, 'new_publication.html')
+
+def settings(request):
+    return render(request, 'settings.html')
