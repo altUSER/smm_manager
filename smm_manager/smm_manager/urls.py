9bf4828b0cc605293f55cbaed83dd6796ac9b1ba
krearden <krearden@yandex.ru>
2023-04-27 20:01:55 +0500

    ༼ つ ◕_◕ ༽つ

--- smm_manager/smm_manager/urls.py
+++ smm_manager/smm_manager/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('settings/', setting_view.settings, name='settings'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('get_json_published_posts/', views.get_json_published_posts, name='get_json_published_posts'),
 ]
--- smm_manager/ui/views.py
+++ smm_manager/ui/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.core import serializers
+
+from ui.models import Post
 
 
 @login_required
@@ -13,3 +17,17 @@ def redirect_to_del_post(request):
 @login_required
 def published_posts(request):
     return render(request, 'published_posts.html')
+
+# def get_json_published_posts(request):
+#     posts = Post.objects.filter(owner = request.user).values()
+#     print(posts)
+#     serialized_objects = serializers.serialize('json', posts)
+#     print(serialized_objects)
+#     return HttpResponse(serialized_objects, content_type='application/json')
+
+def get_json_published_posts(request):
+    posts_data = Post.objects.filter(owner=request.user).values()
+    posts = [Post(**data) for data in posts_data]
+    serialized_objects = serializers.serialize('json', posts)
+    return HttpResponse(serialized_objects, content_type='application/json')
+
